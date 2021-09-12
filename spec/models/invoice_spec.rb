4f5afa12f43c86c93e76b1eccad63ba80a0f404f
Miguel Prada <mapra99@gmail.com>
2021-09-12 15:49:51 -0500

    Increase coverage on invoices

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -29,16 +29,15 @@ RSpec.describe Invoice, type: :model do
 
   context 'hooks' do
     describe '#set_uuid' do
+      let(:user) { create(:user) }
+      let(:client) { create(:client, slug: 'client_slug') }
+      let(:invoice) { create(:invoice, user: user, client: client) }
+
       it 'assigns a uuid based on the client slug and an incremental counter' do
-        user = create(:user)
-        client = create(:client, slug: 'client_slug')
-        invoice = create(:invoice, user: user, client: client)
         expect(invoice.uuid).to eq('client_slug_1')
       end
 
       it 'increases the counter on a user - client basis' do
-        user = create(:user)
-        client = create(:client, slug: 'client_slug')
         invoice1 = create(:invoice, user: user, client: client)
         expect(invoice1.uuid).to eq('client_slug_1')
 
@@ -54,6 +53,14 @@ RSpec.describe Invoice, type: :model do
       it 'doesnt set an uuid if client is blank' do
         invoice = create(:draft_invoice, client: nil)
         expect(invoice.uuid).to be_blank
+      end
+
+      it 'increases the counter until being valid' do
+        create_list(:invoice, 2, user: user, client: client)
+        create(:invoice, user: user, client: client, uuid: 'client_slug_4')
+        invoice4 = create(:invoice, user: user, client: client)
+
+        expect(invoice4.uuid).to eq('client_slug_5')
       end
     end
   end
