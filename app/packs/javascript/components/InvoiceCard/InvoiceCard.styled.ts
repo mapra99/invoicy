194b4bd7fc3bdf145a9b2dc5189307ee30985bb9
Miguel Prada <mapra99@gmail.com>
2021-09-25 18:13:49 -0500

    Fix border color on Invoice card

--- app/packs/javascript/components/InvoiceCard/InvoiceCard.styled.ts
+++ app/packs/javascript/components/InvoiceCard/InvoiceCard.styled.ts
@@ -10,7 +10,7 @@ export const InvoiceCardWrapper = styled.div`
   background: ${props => props.theme.layout.bgSecondary};
   box-shadow: 0px 10px 10px -10px rgba(72, 84, 159, 0.100397);
   border-radius: 8px;
-  border: 1px solid rgba(255, 255, 255, 1);
+  border: 1px solid rgba(255, 255, 255, 0);
   margin-bottom: 16px;
   display: flex;
   flex-wrap: wrap;
