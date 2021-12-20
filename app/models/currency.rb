6697c532add237f43eb2b3e9364728b9ae58bf69
Miguel Prada <mapra99@gmail.com>
2021-12-19 23:00:43 -0500

    Avoid magic string on Currency model

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,4 +1,6 @@
 class Currency < ApplicationRecord
+  DEFAULT_CURRENCY_ABBREVIATION = 'USD'.freeze
+
   has_many :invoices, dependent: :nullify
   has_many :invoice_items, dependent: :nullify
   has_many :items, dependent: :nullify
@@ -6,5 +8,5 @@ class Currency < ApplicationRecord
   validates :abbreviation, presence: true, uniqueness: true
   validates :name, presence: true
 
-  scope :default_currency, -> { find_by(abbreviation: 'USD') }
+  scope :default_currency, -> { find_by(abbreviation: DEFAULT_CURRENCY_ABBREVIATION) }
 end
