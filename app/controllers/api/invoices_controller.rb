935514b4dac2270ef83ada99659b85511e852abe
Miguel Prada <mapra99@gmail.com>
2021-12-19 23:29:13 -0500

    Lint fixes

--- app/controllers/api/invoices_controller.rb
+++ app/controllers/api/invoices_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module Api
   class InvoicesController < BaseController
     before_action :invoice_params, only: [:create]
--- app/services/invoice_creator_service/save_invoice.rb
+++ app/services/invoice_creator_service/save_invoice.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module InvoiceCreatorService
   class SaveInvoice
     include Interactor
