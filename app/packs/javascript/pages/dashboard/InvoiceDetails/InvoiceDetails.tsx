9bcd08e39a852bf799d11d07846ea5755efd4b9d
Miguel Prada <mapra99@gmail.com>
2022-01-02 17:37:04 -0500

    Move GoBackButton to page component

--- app/packs/javascript/pages/dashboard/InvoiceDetails/InvoiceDetails.tsx
+++ app/packs/javascript/pages/dashboard/InvoiceDetails/InvoiceDetails.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { MainContainer } from '../../../components/MainContainer';
+import { GoBackButton } from '../../../components/GoBackButton'
 import { InvoiceDetailsBody } from '../../../components/InvoiceDetailsBody'
 
 export const InvoiceDetails = () => (
   <MainContainer>
+    <GoBackButton />
     <InvoiceDetailsBody />
   </MainContainer>
 );
