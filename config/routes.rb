e71711824c95d9f3468dbb365bf2bcd9eeef0420
Miguel Prada <mapra99@gmail.com>
2022-01-23 22:04:50 -0500

    Setup sidekiq web UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
+require 'sidekiq/web'
+
 # These routes must be consistent with the ones defined in the client apps if needed (app/packs/javascript/constants/routes.ts)
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'react/dashboard/invoices#index'
+  authenticated :user do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
   devise_for :users,
              path: 'users',
